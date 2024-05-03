b29ff18e51bc04ccbd51b7eaa0b5b1981a732179
Samuel Muli <samuelmuli95@gmail.com>
2024-05-03 09:53:22 +0300

    .

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,17 +1,50 @@
+import { useRef } from "react"
 import Input from "./Input"
+import Modal from "./Modal"
 
-export default function NewProject() {
+export default function NewProject({onAdd, onCancel}) {
+    const modal = useRef()
+
+    const title = useRef()
+    const description = useRef()
+    const dueDate = useRef()
+
+    function handleSave() {
+        const enteredTitle = title.current.value
+        const enteredDescription = description.current.value
+        const enteredDueDate = dueDate.current.value
+
+        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === '') {
+            modal.current.open()
+            return
+        }
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        })
+    }
+
+
     return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex justify-end items-center gap-4 my-4">
-                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
-            </menu>
-            <div>
-                <Input label="Title" />
-                <Input label="Description" textarea />
-                <Input label="Due Date" />
+        <>
+            <Modal ref={modal} buttonCaption="Okay">
+                <h2 className="text-xl font-bold text-stone-700 mt-4 my-4">Invalid Input</h2>
+                <p className="text-stone-600 mb-4">Oops...looks like you forgot to enter a value</p>
+                <p className="text-stone-600 mb-4">Please check all fields and try again</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex justify-end items-center gap-4 my-4">
+                    <li><button onClick={onCancel} className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                    <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="Title" />
+                    <Input ref={description} label="Description" textarea />
+                    <Input type="date" ref={dueDate} label="Due Date" />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
